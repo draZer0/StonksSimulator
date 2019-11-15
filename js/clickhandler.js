6e01c41e9cded4c19f5933b81a2ea00cb2528af0
drazero <ardkokkel@outlook.com>
2019-11-15 18:39:51 +0100

    last

--- js/clickhandler.js
+++ js/clickhandler.js
@@ -1,17 +1,21 @@
 let clicks = 0;
 
+
+let button = document.getElementById("button");
+
 onClick = () => {
     clicks += 1;
     document.getElementById("js--clicks").innerHTML = clicks;
 
     if (clicks > 3) {
-      console.log("test");
-      document.getElementById("test").style.display = "block";
-    } else if (clicks < 3) {
-      console.log("nee");
-    } else {
+      console.log("above 3");
+      document.getElementById("item1").style.opacity = "1";
+    };
 
-    }
+    if (clicks > 6) {
+      console.log("above 6");
+      document.getElementById("item2").style.opacity = "1";
+    };
 
 
 }
